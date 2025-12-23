99b3d9d20711009cfa09f310a088a543114d35fb
AliAlzaawawi <alzawawe.ali@gmail.com>
2025-12-23 19:32:07 +0300

    role routers

---
+++ backend/Controllers/role.js
@@ -0,0 +1,81 @@
+const { pool } = require("../Models/db");
+
+const createNewRole = (req, res) => {
+  const { role } = req.body;
+  pool.query(`INSERT INTO roles (role) VALUES ($1) RETURNING * ` , [role])
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: `Role created`,
+        role: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+};
+
+const updateRoleById = (req, res) => {
+    const {roleId} =  req.parms;
+    const { newRole } = req.body;
+  pool.query(`UPDATE roles SET role = $1 WHERE role_id = $2 ` , [newRole , roleId])
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: `Role created`,
+        role: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+};
+
+const createNewPermissions = (req, res) => {
+  const { permission } = req.body;
+  pool.query(`INSERT INTO permissions (permission) VALUES ($1) RETURNING * ` , [permission])
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: `permissions are created`,
+        role: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+}
+const updatePermissionByRole = (req, res) => {
+    const {permissionId} =  req.parms;
+    const { permission } = req.body;
+  pool.query(`UPDATE permissions SET role = $1 WHERE permission_id = $2 ` , [permission , permissionId])
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: `Role created`,
+        role: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+};
+
+
+module.exports = { createNewRole, updateRoleById ,createNewPermissions , updatePermissionByRole };
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 require("dotenv").config();
-const { pool } = require("./db");
+const { pool } = require("./Models/db");
+const rolesRouter = require("./routers/role");
 
+
+app.use("/roles", rolesRouter);
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
